fd1e8921e7b6f10695c5c4a6bbf34b0e38a3ea73
Dinara Idrissova <Dinara.Idrissova@list.ru>
2022-01-22 09:49:29 -0500

    Redirect Users

--- pages/404.js
+++ pages/404.js
@@ -1,11 +1,24 @@
-import  Link  from "next/link";
+import Link from "next/link";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+
 const NotFound = () => {
+  const router = useRouter(); //this retuns as a router object, which contains a method that we can use to redirect the user.
+
+  useEffect(() => {
+    setTimeout(() => {
+      router.push("/");
+    }, 3000);
+    // eslint-disable-next-line
+  }, []);
+
   return (
     <div className="not-found">
       <h1>Oppps....</h1>
       <h2>That page cannot be found</h2>
       <p>
-        Go back to the <Link href="/">
+        Go back to the{" "}
+        <Link href="/">
           <a>Homepage</a>
         </Link>
       </p>
